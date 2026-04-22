0bf96e563d2e168b55f573106031e90f32f70a80
cody norwood <cody.norwood@student.centralia.edu>
2026-04-22 13:21:34 -0700

    Added basecar entity, will work on later

---
+++ src/main/java/production/carsmod/Entity/Custom/BaseCarEntity.java
@@ -0,0 +1,29 @@
+package production.carsmod.Entity.Custom;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.vehicle.VehicleEntity;
+import net.minecraft.item.Item;
+import net.minecraft.storage.ReadView;
+import net.minecraft.storage.WriteView;
+import net.minecraft.world.World;
+
+public class BaseCarEntity extends VehicleEntity {
+    public BaseCarEntity(EntityType<?> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Override
+    protected Item asItem() {
+        return null;
+    }
+
+    @Override
+    protected void readCustomData(ReadView view) {
+
+    }
+
+    @Override
+    protected void writeCustomData(WriteView view) {
+
+    }
+}
